8c92f4a01ab70c9e322719ef940498d14324ce2b
TheBestPedro <142236349+TheBestPedro@users.noreply.github.com>
2023-12-01 10:09:36 +0200

    Add files via upload

---
+++ my website/Pedro.js
@@ -0,0 +1,20 @@
+function openDiscord() {
+    window.open("https://discord.gg/reH6bfqSM9", "_blank");
+}
+
+function openTelegram() {
+    var telegramPopup = window.open("", "_blank", "width=400,height=300");
+    telegramPopup.document.write("<p style='text-align:center; font-size:20px; margin-top:50px;'>@TheBestPedro</p>");
+}
+
+function openXbox() {
+    window.open("https://account.xbox.com/en-us/Profile?xr=mebarnav&rtc=1&csrf=P2pfhojtp4TETC9JhnKdtDyBRCHH58C3nw1q7bt39c1enz6iFUiz9W6gPw0xedujLT_lU863r1cuSiYNVTBPnUS3rLs1&wa=wsignin1.0", "_blank");
+}
+
+function openInstagram() {
+    window.open("https://instagram.com/romsaess?igshid=NzZlODBkYWE4Ng==", "_blank");
+}
+
+function openSteam() {
+    window.open("https://steamcommunity.com/profiles/76561199494077528/", "_blank");
+}
